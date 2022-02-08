958c06ff5da71611d85284ba19e44f26eb143862
unknown <chg8778@gmail.com>
2022-02-07 21:56:48 -0500

    first commit

--- practice.py
+++ practice.py
@@ -4,11 +4,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+# initial setup.
+# all of these should be included
 serv = Service("C:\SeleniumDrivers\chromedriver.exe")
 op = webdriver.ChromeOptions()
 op.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(service=serv, options=op)
 
+
+# get method to open up url
 driver.get("https://www.techwithtim.net/")
 driver.implicitly_wait(10)
 print(driver.title)
@@ -20,8 +24,17 @@ search.send_keys('test')
 search.send_keys(Keys.RETURN)
 driver.implicitly_wait(10)
 
+'''
+find first element with id "main"
+ex) <div id="main">...</div>
+'''
 main = driver.find_element(By.ID, 'main')
 # print(main.text)
+
+'''
+find any html tag with "article" in id main
+ex) <article id="123"> ...... </article>
+'''
 articles = main.find_elements(By.TAG_NAME, "article")
 for article in articles:
     posted_time = article.find_element(By.TAG_NAME, "time")
